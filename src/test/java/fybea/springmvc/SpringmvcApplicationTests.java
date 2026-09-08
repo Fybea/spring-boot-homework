3b768a2ca656b120a6e062d77fd08c946a953b71
Fybea <alexzey.198@gmail.com>
2026-09-08 16:53:31 +0300

    Initial commit

---
+++ src/test/java/fybea/springmvc/SpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package fybea.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
